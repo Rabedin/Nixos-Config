a3b18e6514f2582c015c503c4418dee0daafc477
rabedin <chromeplated@protonmail.com>
2023-11-19 13:28:41 -0500

    added wayland clipboard util

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -42,67 +42,139 @@
     homeDirectory = "/home/red";
   };
 
-  # Kitty
-  programs.kitty = {
-    enable = true;
-    theme = "Catppuccin-Mocha";
-  };
-
-  # Neovim
-  programs.neovim = {
-    enable = true;
-    vimAlias = true;
-  };
-
-  # Zsh
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      updateconfig = "sudo nixos-rebuild switch --flake ~/nixos-config/#nixos";
-      updatehome = "home-manager switch --flake ~/nixos-config/#red@nixos";
-    };
-    # histSize = 10000;
-    # histFile = "${config.xdg.dataHome}/zsh/history";
-    oh-my-zsh = {
+  # Programs
+  programs = {
+    # chromium
+    chromium = {
       enable = true;
-      plugins = [  ];
-      theme = "agnoster";
+      package = pkgs.ungoogled-chromium;
+      extensions =
+      let
+        createChromiumExtensionFor = browserVersion: { id, sha256, version }:
+          {
+            inherit id;
+            crxPath = builtins.fetchurl {
+              url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${browserVersion}&x=id%3D${id}%26installsource%3Dondemand%26uc";
+              name = "${id}.crx";
+              inherit sha256;
+            };
+            inherit version;
+          };
+        createChromiumExtension = createChromiumExtensionFor (lib.versions.major pkgs.ungoogled-chromium.version);
+      in 
+      [
+        (createChromiumExtension {
+          # ublock origin
+          id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+          sha256 = "sha256:1y5bcba6w97mdq0cmw75k25rw7fxnlyy4165vls7vf6xkw9aqy3z";
+          version = "1.53.0";
+         })
+        (createChromiumExtension {
+          # proton pass
+          id = "ghmbeldphafepmbegfdlkpapadhbakde";
+          sha256 = "sha256:1yy03kix9zi1xwnhd7v3bi8q53yhf5h0yhk1b1k53j8xymkydhwl";
+          version = "1.8.4";
+         })
+      ];
+    };
+    # git
+    git = {
+      enable = true;
+      aliases = {
+        co = "checkout";
+        cm = "commit";
+      };
+      userName = "rabedin";
+      userEmail = "chromeplated@protonmail.com";
+    };
+    # kitty
+    kitty = {
+      enable = true;
+      theme = "Catppuccin-Mocha";
+    };
+    # neovim
+    neovim = {
+      enable = true;
+      vimAlias = true;
+    };
+    # zsh
+    zsh = {
+      enable = true;
+      shellAliases = {
+        updateconfig = "sudo nixos-rebuild switch --flake ~/nixos-config/#nixos";
+        updatehome = "home-manager switch --flake ~/nixos-config/#red@nixos";
+      };
+      # histSize = 10000;
+      # histFile = "${config.xdg.dataHome}/zsh/history";
+      oh-my-zsh = {
+        enable = true;
+        plugins = [  ];
+        theme = "agnoster";
+      };
     };
   };
 
-  # Ungoogled-chromium stuff
-  programs.chromium = {
-    enable = true;
-    package = pkgs.ungoogled-chromium;
-    extensions =
-    let
-      createChromiumExtensionFor = browserVersion: { id, sha256, version }:
-        {
-          inherit id;
-          crxPath = builtins.fetchurl {
-            url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${browserVersion}&x=id%3D${id}%26installsource%3Dondemand%26uc";
-            name = "${id}.crx";
-            inherit sha256;
-          };
-          inherit version;
-        };
-      createChromiumExtension = createChromiumExtensionFor (lib.versions.major pkgs.ungoogled-chromium.version);
-    in 
-    [
-      (createChromiumExtension {
-        # ublock origin
-        id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
-        sha256 = "sha256:1y5bcba6w97mdq0cmw75k25rw7fxnlyy4165vls7vf6xkw9aqy3z";
-        version = "1.53.0";
-       })
-      (createChromiumExtension {
-        # proton pass
-        id = "ghmbeldphafepmbegfdlkpapadhbakde";
-        sha256 = "sha256:1yy03kix9zi1xwnhd7v3bi8q53yhf5h0yhk1b1k53j8xymkydhwl";
-        version = "1.8.4";
-       })
-    ];
-  };
+  # # Kitty
+  # programs.kitty = {
+  #   enable = true;
+  #   theme = "Catppuccin-Mocha";
+  # };
+  #
+  # # Neovim
+  # programs.neovim = {
+  #   enable = true;
+  #   vimAlias = true;
+  # };
+  #
+  # # Zsh
+  # programs.zsh = {
+  #   enable = true;
+  #   shellAliases = {
+  #     updateconfig = "sudo nixos-rebuild switch --flake ~/nixos-config/#nixos";
+  #     updatehome = "home-manager switch --flake ~/nixos-config/#red@nixos";
+  #   };
+  #   # histSize = 10000;
+  #   # histFile = "${config.xdg.dataHome}/zsh/history";
+  #   oh-my-zsh = {
+  #     enable = true;
+  #     plugins = [  ];
+  #     theme = "agnoster";
+  #   };
+  # };
+  #
+  # # Ungoogled-chromium stuff
+  # programs.chromium = {
+  #   enable = true;
+  #   package = pkgs.ungoogled-chromium;
+  #   extensions =
+  #   let
+  #     createChromiumExtensionFor = browserVersion: { id, sha256, version }:
+  #       {
+  #         inherit id;
+  #         crxPath = builtins.fetchurl {
+  #           url = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${browserVersion}&x=id%3D${id}%26installsource%3Dondemand%26uc";
+  #           name = "${id}.crx";
+  #           inherit sha256;
+  #         };
+  #         inherit version;
+  #       };
+  #     createChromiumExtension = createChromiumExtensionFor (lib.versions.major pkgs.ungoogled-chromium.version);
+  #   in 
+  #   [
+  #     (createChromiumExtension {
+  #       # ublock origin
+  #       id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+  #       sha256 = "sha256:1y5bcba6w97mdq0cmw75k25rw7fxnlyy4165vls7vf6xkw9aqy3z";
+  #       version = "1.53.0";
+  #      })
+  #     (createChromiumExtension {
+  #       # proton pass
+  #       id = "ghmbeldphafepmbegfdlkpapadhbakde";
+  #       sha256 = "sha256:1yy03kix9zi1xwnhd7v3bi8q53yhf5h0yhk1b1k53j8xymkydhwl";
+  #       version = "1.8.4";
+  #      })
+  #   ];
+  # };
 
   # Hyprland
   wayland.windowManager.hyprland.extraConfig = ''
@@ -260,6 +332,7 @@ bindm = $mainMod, mouse:273, resizewindow
 
   # Add stuff for your user as you see fit:
   home.packages = with pkgs; [
+    wl-clipboard
     gnumake
     iosevka
     nerdfonts
@@ -275,7 +348,6 @@ bindm = $mainMod, mouse:273, resizewindow
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
